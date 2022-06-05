c3c3194a1fd16fd48401460dc2954ae4a29fb945
Woongcheol Shin <1994_100@naver.com>
2022-06-05 23:55:25 +0900

    create android and splash

---
+++ app/src/main/java/com/mm/android/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.mm.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen() // Activiy 확장함수
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
