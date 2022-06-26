ed456d7662bd2fed769ad718d1a6b4174f57dbb5
Woongcheol Shin <1994_100@naver.com>
2022-06-26 13:09:46 +0900

    update package

--- app/src/main/java/com/mm/android/Entry.kt
+++
@@ -1,3 +0,0 @@
-package com.mm.android
-
-data class Entry(val x: Int, val y:Int)
--- app/src/main/java/com/mm/android/News.kt
+++
@@ -1,3 +0,0 @@
-package com.mm.android
-
-data class News(var image: String, var title: String, var content: String, var newsPaper: String, var time: Long, val url: String)
