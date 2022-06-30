0246587926cbd839aee80b1ca1ec8aaf062cb402
Woongcheol Shin <1994_100@naver.com>
2022-06-30 23:42:25 +0900

    update Search(UX/UI)

--- app/src/main/java/com/mm/android/searchResult/adapcter/SearchResultAdapter.kt
+++ app/src/main/java/com/mm/android/searchResult/adapcter/SearchResultAdapter.kt
@@ -3,16 +3,17 @@ package com.mm.android.searchResult.adapcter
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.mm.android.databinding.ItemRecyclerBinding
 import com.mm.android.itemdetail.ItemDetailActivity
 import com.mm.android.searchResult.data.ItemRank
-import com.mm.android.itemdetail.data.DetailItem
+import com.mm.android.sub.DialogActivity
 
 class SearchResultAdapter : RecyclerView.Adapter<SearchResultAdapter.Holder>() {
-    lateinit var listData:ArrayList<ItemRank>
+    lateinit var listData: ArrayList<ItemRank>
     var bundle = Bundle()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
@@ -28,15 +29,16 @@ class SearchResultAdapter : RecyclerView.Adapter<SearchResultAdapter.Holder>() {
     override fun onBindViewHolder(holder: Holder, position: Int) {
         val market = listData.get(position)
         holder.setMarket(market)
+        val percent = holder.binding.changeText.text.toString()
 
         // 변동률에 따른 색상 변경
-//        if (holder.binding.changeText.text.toString().toDouble() > 0) {
-//            holder.binding.changeText.setTextColor(Color.rgb(81,107,244))
-//            holder.binding.changeImage.setColorFilter(Color.parseColor("#E8F2FF"))
-//        } else {
-//            holder.binding.changeText.setTextColor(Color.rgb(244,81,81))
-//            holder.binding.changeImage.setColorFilter(Color.parseColor("#FFE8E8"))
-//        }
+        if (percent.replace("%","").toDouble() > 0) {
+            holder.binding.changeText.setTextColor(Color.rgb(81,107,244))
+            holder.binding.changeImage.setColorFilter(Color.parseColor("#E8F2FF"))
+        } else {
+            holder.binding.changeText.setTextColor(Color.rgb(244,81,81))
+            holder.binding.changeImage.setColorFilter(Color.parseColor("#FFE8E8"))
+        }
     }
 
     override fun getItemCount(): Int {
@@ -46,7 +48,9 @@ class SearchResultAdapter : RecyclerView.Adapter<SearchResultAdapter.Holder>() {
     // intent 구현을 위해 parent를 인자로 받음, inner class로 구현
     inner class Holder(val binding: ItemRecyclerBinding, parent: ViewGroup) :
 
+
         RecyclerView.ViewHolder(binding.root) {
+        lateinit var dialog: DialogActivity
 
         init {
             val context = parent.context
@@ -55,12 +59,15 @@ class SearchResultAdapter : RecyclerView.Adapter<SearchResultAdapter.Holder>() {
                 val intent = Intent(context, ItemDetailActivity::class.java)
 
 //              액티비티로 인텐트 전달, adapterposition - 리싸이클러뷰에서 제공하는 위치 제공 메소드, https://velog.io/@appletorch/%EB%A6%AC%EC%82%AC%EC%9D%B4%ED%81%B4%EB%9F%AC%EB%B7%B0-%EC%95%84%EC%9D%B4%ED%85%9C-%ED%81%B4%EB%A6%AD-%EC%9D%B4%EB%B2%A4%ED%8A%B8
-                val item = listData.get(adapterPosition).pgroupName
+                val pGroup = listData.get(adapterPosition).pgroupName!!
+
+                // 로딩화면 생성
 
                 // retrofit 통신 보내기
-                intent.putExtra("test", "${item}")
-                context.startActivity(intent)
-
+                Log.d("test pGroup", "$pGroup")
+                dialog = DialogActivity(parent.context)
+                parent.context.startActivity(intent)
+//                getDetailItem(intent, pGroup, parent)
             }
         }
 
@@ -70,6 +77,34 @@ class SearchResultAdapter : RecyclerView.Adapter<SearchResultAdapter.Holder>() {
             binding.item.text = "${itemRank.pgroupName}"
             binding.changeText.text = "${itemRank.percentage}"
         }
+
+//        private fun getDetailItem(intent: Intent, pGroup: String, parent:ViewGroup) {
+//            val retrofitAPI = RetrofitConnection.getInstance().create(RetrofitService::class.java)
+//
+//            retrofitAPI.getDetailItemList(pGroup)
+//                .enqueue(object : Callback<DetailItemListResponse> {
+//                    override fun onResponse(
+//                        call: Call<DetailItemListResponse>,
+//                        response: Response<DetailItemListResponse>
+//                    ) {
+//                        dialog.show()
+//                        if (response.isSuccessful) {
+//                            val data = response.body()
+//                            Log.d("test data", "$data")
+//                            intent.putStringArrayListExtra("itemDetail", data)
+//                            dialog.dismiss()
+//                            parent.context.startActivity(intent)
+//                        } else {
+//                            Toast.makeText(parent.context, "랭킹 서비스 실행 실패", Toast.LENGTH_LONG).show()
+//                        }
+//
+//                    }
+//
+//                    override fun onFailure(call: Call<DetailItemListResponse>, t: Throwable) {
+//                        t.printStackTrace()
+//                    }
+//                })
+//        }
 
     }
 }
