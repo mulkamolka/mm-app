109472a7fcbf340bb337066aa8943551768862a9
Woongcheol Shin <1994_100@naver.com>
2022-06-30 21:49:17 +0900

    update Home Retrofit

--- app/src/main/java/com/mm/android/home/fragment/HomeCategoryFragment.kt
+++ app/src/main/java/com/mm/android/home/fragment/HomeCategoryFragment.kt
@@ -6,15 +6,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.mm.android.category.CategoryAActivity
-import com.mm.android.category.CategoryBActivity
-import com.mm.android.category.CategoryCActivity
-import com.mm.android.category.CategoryDActivity
+import android.widget.Toast
+import com.mm.android.searchResult.CategoryAActivity
+import com.mm.android.searchResult.CategoryBActivity
+import com.mm.android.searchResult.CategoryCActivity
+import com.mm.android.searchResult.CategoryDActivity
+import com.mm.android.searchResult.data.CategoryItemRankListResponse
 import com.mm.android.databinding.FragmentHomeCategoryBinding
+import com.mm.android.retrofit.RetrofitService
+import com.mm.android.retrofit.RetrofitConnection
+import com.mm.android.sub.DialogActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class HomeCategoryFragment : Fragment() {
     private var _binding: FragmentHomeCategoryBinding? = null
     private val binding get() = _binding!!
+    lateinit var dialog: DialogActivity
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,32 +33,69 @@ class HomeCategoryFragment : Fragment() {
         _binding = FragmentHomeCategoryBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        // 로딩화면 생성
+        dialog = DialogActivity(container!!.context)
+
         binding.categoryA.setOnClickListener {
+            dialog.show()
             val intent = Intent(activity, CategoryAActivity::class.java)
-            startActivity(intent)
+            getCategoryItemRank(intent, "agric")
         }
+
 
         binding.categoryB.setOnClickListener {
             val intent = Intent(activity, CategoryBActivity::class.java)
+            getCategoryItemRank(intent, binding.categoryB.text.toString())
             startActivity(intent)
         }
 
         binding.categoryC.setOnClickListener {
             val intent = Intent(activity, CategoryCActivity::class.java)
+            getCategoryItemRank(intent, binding.categoryC.text.toString())
             startActivity(intent)
         }
 
         binding.categoryD.setOnClickListener {
             val intent = Intent(activity, CategoryDActivity::class.java)
+            getCategoryItemRank(intent, binding.categoryD.text.toString())
             startActivity(intent)
         }
 
         return view
     }
 
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun getCategoryItemRank(intent: Intent, category: String) {
+        val retrofitAPI = RetrofitConnection.getInstance().create(RetrofitService::class.java)
+
+        retrofitAPI.getCategoryItemRankData(category)
+            .enqueue(object : Callback<CategoryItemRankListResponse> {
+                override fun onResponse(
+                    call: Call<CategoryItemRankListResponse>,
+                    response: Response<CategoryItemRankListResponse>
+                ) {
+                    if (response.isSuccessful) {
+                        Toast.makeText(context, "랭킹 서비스 실행 완료", Toast.LENGTH_LONG).show()
+                        val data = response.body()
+                        intent.putExtra("itemRankListData", data)
+                        dialog.dismiss()
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(context, "랭킹 서비스 실행 실패", Toast.LENGTH_LONG).show()
+                    }
+
+                }
+
+                override fun onFailure(call: Call<CategoryItemRankListResponse>, t: Throwable) {
+                    t.printStackTrace()
+                    Toast.makeText(context, "랭킹 서비스 실행 실패", Toast.LENGTH_LONG).show()
+                }
+            })
     }
 
 }
--- app/src/main/java/com/mm/android/home/fragment/HomeSearchFragment.kt
+++ app/src/main/java/com/mm/android/home/fragment/HomeSearchFragment.kt
@@ -6,12 +6,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.mm.android.category.SearchActivity
+import android.widget.Toast
+import com.mm.android.searchResult.SearchActivity
+import com.mm.android.searchResult.data.AllItemRankListResponse
 import com.mm.android.databinding.FragmentHomeSearchBinding
+import com.mm.android.retrofit.RetrofitService
+import com.mm.android.retrofit.RetrofitConnection
+import com.mm.android.sub.DialogActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import kotlin.concurrent.thread
 
 class HomeSearchFragment : Fragment() {
     private var _binding: FragmentHomeSearchBinding? = null
     private val binding get() = _binding!!
+    lateinit var dialog: DialogActivity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,18 +29,51 @@ class HomeSearchFragment : Fragment() {
     ): View? {
         _binding = FragmentHomeSearchBinding.inflate(inflater, container, false)
         val view = binding.root
+        val intent = Intent(activity, SearchActivity::class.java)
+
+        // 로딩화면 생성
+        dialog = DialogActivity(container!!.context)
 
         binding.searchButton.setOnClickListener {
-            val intent = Intent(activity, SearchActivity::class.java)
-            startActivity(intent)
+            dialog.show()
+            getAllItemRank(intent)
         }
 
         return view
     }
 
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun getAllItemRank(intent: Intent) {
+        val retrofitAPI = RetrofitConnection.getInstance().create(RetrofitService::class.java)
+
+        retrofitAPI.getAllItemRankData()
+            .enqueue(object : Callback<AllItemRankListResponse> {
+                override fun onResponse(
+                    call: Call<AllItemRankListResponse>,
+                    response: Response<AllItemRankListResponse>
+                ) {dialog.show()
+                    if (response.isSuccessful) {
+                        Toast.makeText(context, "랭킹 서비스 실행 완료", Toast.LENGTH_LONG).show()
+                        val data = response.body()
+                        intent.putExtra("itemRankListData", data)
+                        dialog.dismiss()
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(context, "랭킹 서비스 실행 실패", Toast.LENGTH_LONG).show()
+                    }
+
+                }
+
+                override fun onFailure(call: Call<AllItemRankListResponse>, t: Throwable) {
+                    t.printStackTrace()
+                    Toast.makeText(context, "랭킹 서비스 실행 실패", Toast.LENGTH_LONG).show()
+                }
+            })
     }
 
 }
