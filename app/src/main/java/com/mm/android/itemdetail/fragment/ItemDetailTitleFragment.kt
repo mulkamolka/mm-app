88338de2d72f8775a8e5f44d3fe7af40a86c17ba
Woongcheol Shin <1994_100@naver.com>
2022-07-05 10:53:29 +0900

    add Spinner Data from Server

--- app/src/main/java/com/mm/android/itemdetail/fragment/ItemDetailTitleFragment.kt
+++ app/src/main/java/com/mm/android/itemdetail/fragment/ItemDetailTitleFragment.kt
@@ -12,12 +12,12 @@ import com.mm.android.R
 import com.mm.android.home.MainActivity
 import com.mm.android.databinding.FragmentItemDetailTitleBinding
 import com.mm.android.itemdetail.data.DetailItem
+import com.mm.android.searchResult.data.ItemRank
 
 class ItemDetailTitleFragment : Fragment() {
 
     private var _binding: FragmentItemDetailTitleBinding? = null
     private val binding get() = _binding!!
-
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,14 +28,27 @@ class ItemDetailTitleFragment : Fragment() {
         val view = binding.root
         val backButton = binding.backButton
 
-        // 데이터를 불러온다
-        val data = loadData()
+        // 데이터 읽기 및 스피너 데이터 입력
+        val data = loadData() // 트러블 슈팅 1 - 여기에서는 null 값으로 뜸
         val spinnerData = ArrayList<String>()
-        for (i in 0..data.size-1) {
-            spinnerData.add(data.get(i).Item)
+        for (i in 0..data.size - 1) {
+            val temp = data.get(i)
+            val desc = data.get(i).description
+            val descData = loadDesc(desc!!)
+            if (descData != temp.unit) {
+                spinnerData.add("${temp.kind + " / " + temp.rank + " / " + temp.unit + " / " + descData}")
+            } else {
+                spinnerData.add("${temp.kind + " / " + temp.rank + " / " + temp.unit}")
+            }
         }
+        Log.d("test spinnerData", "${spinnerData}")
 
-        val adapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_list_item_1, spinnerData)
+        // 데이터 읽기 및 itemName 변경
+        binding.detailItemName.text = arguments?.getString("pGroup")
+
+
+        val adapter =
+            ArrayAdapter<String>(requireContext(), R.layout.custom_list_item_1, spinnerData)
         binding.spinner.adapter = adapter
 
         // 뒤로가기 버튼
@@ -52,14 +65,19 @@ class ItemDetailTitleFragment : Fragment() {
     }
 
     // 임시 함수
-    fun loadData() : ArrayList<DetailItem>{
-        val data = ArrayList<DetailItem>()
-        data.add(DetailItem("상추"))
-        data.add(DetailItem("배추"))
-        data.add(DetailItem("양배추"))
-
-        return data
+    private fun loadData(): ArrayList<DetailItem> {
+        val tempData = arguments?.getSerializable("itemDetailListData") as ArrayList<DetailItem>
+        Log.d("test itemDetailListData", "${tempData}")
+        return tempData
     }
 
+    private fun loadDesc(data: String): String {
+        val descData: String
+        val splitData = data.split('(')[1]
+        descData = splitData.split(')')[0]
+
+        Log.d("test loadDesc()", "${descData}")
+        return descData
+    }
 
 }
